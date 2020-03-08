b3bcc8ae9f76bb204a05c1c89610ae7c56013806
Maksym Fedorenko <maksym.fedorenko@ticketmaster.com>
2020-03-08 08:51:56 +0200

    Initial commit

---
+++ src/main/java/com/ximeo/smartstring/initiator/Initiator.java
@@ -0,0 +1,375 @@
+package com.ximeo.smartstring.initiator;
+
+import java.math.BigDecimal;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Initiator {
+    private Random random = new Random();
+
+    public static void main(String[] args) {
+        Initiator actions = new Initiator();
+//        actions.sayHello();
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                String question = "";
+                boolean isTheLastWordInTheQuestion = false;
+                while (!isTheLastWordInTheQuestion) {
+                    if (!question.isEmpty()) question += " ";
+                    question += scanner.next();
+                    if (question.endsWith(".") || question.endsWith("?") || question.endsWith("!")) isTheLastWordInTheQuestion = true;
+                }
+//                System.out.println(question);
+//                actions.defineCommandName(actions.doLexicalAnalysys(question));
+//                actions.getCommand(question);
+//                actions.commandDispatcher(actions.interpretateToCommandName(question));
+                actions.commandDispatcher(actions.getCommand(question));
+            } catch (Exception ex) {
+                System.out.println("Извините, произошла непредвиденная ситуация");
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private String interpretateToCommandName(String str) {
+        return getCommandName(str);
+    }
+
+    private List<String> parseAString(String aString) {
+        String[] res = aString.split(" ");
+        List<String> toSend = new ArrayList<>();
+        for (String a : res) {
+            toSend.add(a.toLowerCase());
+        }
+        return toSend;
+    }
+
+    private void commandDispatcher(String commandName) {
+        switch (commandName) {
+            case COMMAND_SAY_GREETING: sayHello();
+                break;
+            case COMMAND_SAY_HOW_YOU_ARE: sayHowAreYou();
+                break;
+            case COMMAND_SAY_WEATHER: sayTheWeather();
+                break;
+            case COMMAND_SAY_CURRENCY: sayCurrency();
+                break;
+            case COMMAND_SAY_CURRENT_TIME: sayTime();
+                break;
+            case COMMAND_SAY_CURRENT_DATE: sayDate();
+                break;
+            case COMMAND_SAY_GOODBYE: sayBye();
+                break;
+            default: sayIDontKnow();
+        }
+    }
+
+    private void sayHello() {
+        Set<String> answ = getAnswers().get("Приветствие");
+        int numb = random.nextInt(answ.size());
+        List<String> sdf = new ArrayList<>(answ);
+        System.out.println(sdf.get(numb));
+    }
+
+    private void sayHowAreYou() {
+        Set<String> answ = getAnswers().get("Состояние_дел");
+        int numb = random.nextInt(answ.size());
+        List<String> sdf = new ArrayList<>(answ);
+        System.out.println(sdf.get(numb));
+    }
+
+    private void sayTheWeather() {
+        Set<String> answ = getAnswers().get("Погода");
+        int numb = random.nextInt(answ.size());
+        List<String> sdf = new ArrayList<>(answ);
+        System.out.println(sdf.get(numb));
+    }
+
+    private void sayCurrency() {
+        int numb = random.nextInt(2000);
+        BigDecimal bd = new BigDecimal(numb);
+        BigDecimal res = (bd.divide(new BigDecimal(1000))).add(new BigDecimal(22));
+        System.out.println("На данный момент курс доллара: " + res + "$");
+    }
+
+    private void sayTime() {
+        System.out.println(LocalTime.now());
+    }
+
+    private void sayDate() {
+        System.out.print(LocalDate.now());
+    }
+
+    private void sayIDontKnow() {
+        Set<String> answ = getAnswers().get("Неизвестное");
+        int numb = random.nextInt(answ.size());
+        List<String> sdf = new ArrayList<>(answ);
+        System.out.println(sdf.get(numb));
+    }
+
+    private void sayBye() {
+        Set<String> answ = getAnswers().get("Прощание");
+        int numb = random.nextInt(answ.size());
+        List<String> sdf = new ArrayList<>(answ);
+        System.out.println(sdf.get(numb));
+        System.exit(0);
+    }
+
+    private String defineCommandName(Set<String> tags) {
+        Map<Set<String>, String> lexems = getTagsMap();
+        Set<Map.Entry<Set<String>, String>> sddffs = lexems.entrySet();
+        for (Map.Entry<Set<String>, String> str : sddffs) {
+            Set<String> sets = str.getKey();
+            for (String st : sets) {
+                if(tags.contains(st)) return str.getValue();
+            }
+        }
+        return "";
+    }
+
+    private Set<String> doLexicalAnalysys(String sentense) {
+        Set<String> tags = getTags();
+        Set<String> toSend = new HashSet<>();
+        sentense = sentense.toLowerCase();
+        for (String tag : tags) {
+            if (sentense.contains(tag)) toSend.add(tag);
+        }
+        return toSend;
+    }
+
+    private Set<String> getTags() {
+        Set<String> toSend = new HashSet<>();
+        toSend.add(TOKEN_HELLO_1);
+        toSend.add(TOKEN_HELLO_2);
+        toSend.add(TOKEN_HELLO_3);
+        toSend.add(TOKEN_WEATHER_1);
+        toSend.add(TOKEN_WEATHER_2);
+        toSend.add(TOKEN_HOW_ARE_YOU_1);
+        toSend.add(TOKEN_HOW_ARE_YOU_2);
+        toSend.add(TOKEN_HOW_ARE_YOU_3);
+        toSend.add(TOKEN_DATE_1);
+        toSend.add(TOKEN_DATE_2);
+        toSend.add(TOKEN_BYE_1);
+        toSend.add(TOKEN_BYE_2);
+        toSend.add(TOKEN_BYE_3);
+        toSend.add(TOKEN_CURRENCY_1);
+        toSend.add(TOKEN_CURRENCY_2);
+        toSend.add(TOKEN_TIME_1);
+        toSend.add(TOKEN_TIME_2);
+        return toSend;
+    }
+
+    private static final String TOKEN_HELLO_1 = "прив";
+    private static final String TOKEN_HELLO_2 = "добр";
+    private static final String TOKEN_HELLO_3 = "здравст";
+    private static final String TOKEN_WEATHER_1 = "погод";
+    private static final String TOKEN_WEATHER_2 = "темпер";
+    private static final String TOKEN_HOW_ARE_YOU_1 = "дел";
+    private static final String TOKEN_HOW_ARE_YOU_2 = "как ты";
+    private static final String TOKEN_HOW_ARE_YOU_3 = "как он";
+    private static final String TOKEN_DATE_1 = "дат";
+    private static final String TOKEN_DATE_2 = "числ";
+    private static final String TOKEN_BYE_1 = "пок";
+    private static final String TOKEN_BYE_2 = "проща";
+    private static final String TOKEN_BYE_3 = "до свид";
+    private static final String TOKEN_CURRENCY_1 = "курс";
+    private static final String TOKEN_CURRENCY_2 = "дол";
+    private static final String TOKEN_TIME_1 = "врем";
+    private static final String TOKEN_TIME_2 = "час";
+
+
+    private String getCommandName(String str) {
+//        System.err.println(str);
+        Map<Set<String>, String> sdf = getTagsMap();
+        Set<Map.Entry<Set<String>, String>> cbcb = sdf.entrySet();
+        for (Map.Entry<Set<String>, String> ab : cbcb) {
+            Set<String> s1 = ab.getKey();
+            for (String ss : s1) {
+//                System.err.println(str + " == " + ss + " : " + str.equalsIgnoreCase(ss));
+                if (str.equalsIgnoreCase(ss)) {
+                    return ab.getValue();
+                }
+            }
+        }
+        return "";
+    }
+
+    private String getCommand(String ssss) {
+        Set<String> strs = getTagsSet(ssss);
+        System.err.println(strs.size());
+        Map<Set<String>, String> mapa = getCommandMap();
+        Set<Map.Entry<Set<String>, String>> st = mapa.entrySet();
+        Map<Set<String>, String> ss = new HashMap<>();
+        for (Map.Entry<Set<String>, String> s : st) {
+            Set<String> dff = s.getKey();
+            Set<String> localSet = new HashSet<>();
+            for (String sf : dff) {
+                if (strs.contains(sf)) localSet.add(sf);
+            }
+            if (localSet.size() == dff.size()) return s.getValue();
+            ss.put(localSet, s.getValue());
+        }
+        int maxSuits = 0;
+        String toSend = null;
+        Set<Map.Entry<Set<String>, String>> sfgs = ss.entrySet();
+        for (Map.Entry<Set<String>, String> sfg : sfgs) {
+            if (sfg.getKey().size() > maxSuits) {
+                maxSuits = sfg.getKey().size();
+                toSend = sfg.getValue();
+            }
+        }
+        return toSend;
+    }
+
+    private Set<String> getTagsSet(String str) {
+        Map<Set<String>, String> sdf = getTagsMap();
+        System.err.println(sdf);
+        Set<Map.Entry<Set<String>, String>> cbcb = sdf.entrySet();
+        Set<String> toSend = new HashSet<>();
+        for (Map.Entry<Set<String>, String> ab : cbcb) {
+            Set<String> s1 = ab.getKey();
+            for (String ss : s1) {
+                if (str.contains(ss)) {
+                    toSend.add(ab.getValue());
+                }
+            }
+        }
+        return toSend;
+    }
+
+    private static final String TAG_GREETING = "hello";
+    private static final String TAG_STATE_OF_AFFAIRS = "routine";
+    private static final String TAG_EXCHANGE_RATE = "currency";
+    private static final String TAG_DATE = "date";
+    private static final String TAG_WEATHER = "weather";
+    private static final String TAG_TIME = "time";
+    private static final String TAG_PARTING = "bye";
+
+    private static final String COMMAND_SAY_WEATHER = "weather";
+    private static final String COMMAND_SAY_GREETING = "hello";
+    private static final String COMMAND_SAY_HOW_YOU_ARE = "routine";
+    private static final String COMMAND_SAY_CURRENCY = "currency";
+    private static final String COMMAND_SAY_CURRENT_TIME = "time";
+    private static final String COMMAND_SAY_CURRENT_DATE = "date";
+    private static final String COMMAND_SAY_GOODBYE = "bye";
+
+    private Map<Set<String>, String> getCommandMap() {
+        Map<Set<String>, String> commandMap = new HashMap<>();
+        commandMap.put(Set.of(TAG_GREETING), COMMAND_SAY_GREETING);
+        commandMap.put(Set.of(TAG_STATE_OF_AFFAIRS), COMMAND_SAY_HOW_YOU_ARE);
+        commandMap.put(Set.of(TAG_EXCHANGE_RATE), COMMAND_SAY_CURRENCY);
+        commandMap.put(Set.of(TAG_DATE), COMMAND_SAY_CURRENT_DATE);
+        commandMap.put(Set.of(TAG_WEATHER), COMMAND_SAY_WEATHER);
+        commandMap.put(Set.of(TAG_TIME), COMMAND_SAY_CURRENT_TIME);
+        commandMap.put(Set.of(TAG_PARTING), COMMAND_SAY_GOODBYE);
+        return commandMap;
+    }
+
+    private Map<Set<String>, String> getTagsMap() {
+        Map<Set<String>, String> tagsMap = new HashMap<>();
+        tagsMap.put(Set.of(TOKEN_HELLO_1, TOKEN_HELLO_2, TOKEN_HELLO_3), TAG_GREETING);
+        tagsMap.put(Set.of(TOKEN_HOW_ARE_YOU_1, TOKEN_HOW_ARE_YOU_2, TOKEN_HOW_ARE_YOU_3),
+            TAG_STATE_OF_AFFAIRS);
+        tagsMap.put(Set.of(TOKEN_CURRENCY_1, TOKEN_CURRENCY_2), TAG_EXCHANGE_RATE);
+        tagsMap.put(Set.of(TOKEN_DATE_1, TOKEN_DATE_2), TAG_DATE);
+        tagsMap.put(Set.of(TOKEN_WEATHER_1, TOKEN_WEATHER_2), TAG_WEATHER);
+        tagsMap.put(Set.of(TOKEN_TIME_1, TOKEN_TIME_2), TAG_TIME);
+        tagsMap.put(Set.of(TOKEN_BYE_1, TOKEN_BYE_2, TOKEN_BYE_3), TAG_PARTING);
+        return tagsMap;
+    }
+
+//    private Map<Set<String>, String> getCommandMap() {
+//        Map<Set<String>, String> commandMap = new HashMap<>();
+//        Set<String> str0 = new HashSet<>();
+//        str0.add("привет!");
+//        str0.add("здравствуй!");
+//        str0.add("hello!");
+//        str0.add("hi!");
+//        str0.add("хай!");
+//        str0.add("здорова!");
+//        commandMap.put(str0, "hello");
+//        Set<String> str1 = new HashSet<>();
+//        str1.add("как дела?");
+//        str1.add("как оно?");
+//        str1.add("как ты?");
+//        commandMap.put(str1, "routine");
+//        Set<String> str2 = new HashSet<>();
+//        str2.add("какая погода?");
+//        commandMap.put(str2, "weather");
+//        Set<String> str3 = new HashSet<>();
+//        str3.add("какой курс сегодня?");
+//        str3.add("сколько стоит доллар?");
+//        str3.add("по чем нынче бакс?");
+//        commandMap.put(str3, "currency");
+//        Set<String> str4 = new HashSet<>();
+//        str4.add("какое число сегодня?");
+//        str4.add("какое сегодня число?");
+//        commandMap.put(str4, "date");
+//        Set<String> str5 = new HashSet<>();
+//        str5.add("сколько времени?");
+//        str5.add("который час?");
+//        str5.add("какое время сейчас?");
+//        commandMap.put(str5, "time");
+//        Set<String> str6 = new HashSet<>();
+//        str6.add("прощай!");
+//        str6.add("Прощай!");
+//        str6.add("Аривидерчи!");
+//        str6.add("Чао!");
+//        str6.add("До встречи!");
+//        str6.add("Гудбай!");
+//        str6.add("До свидания!");
+//        commandMap.put(str6, "bye");
+//        return commandMap;
+//    }
+
+    private Map<String, Set<String>> getAnswers() {
+        Map<String, Set<String>> answersMap = new HashMap<>();
+        Set<String> howAreYouSet = new HashSet<>();
+        howAreYouSet.add("Хорошо!");
+        howAreYouSet.add("Спасибо, хорошо!");
+        howAreYouSet.add("Бывало и лучше!");
+        howAreYouSet.add("Также как и вчера!");
+        howAreYouSet.add("Ничего");
+        answersMap.put("Состояние_дел", howAreYouSet);
+        Set<String> helloSet = new HashSet<>();
+        helloSet.add("Привет!");
+        helloSet.add("Здравствуйте!");
+        helloSet.add("Доброго времени суток!");
+        answersMap.put("Приветствие", helloSet);
+        Set<String> weather = new HashSet<>();
+        weather.add("Тепло");
+        weather.add("Жарко");
+        weather.add("Сегодня, прохладно");
+        weather.add("Прохладно");
+        weather.add("Солнечно");
+        weather.add("Холодно");
+        answersMap.put("Погода", weather);
+        Set<String> parting = new HashSet<>();
+        parting.add("Пока!");
+        parting.add("Прощай!");
+        parting.add("Аривидерчи!");
+        parting.add("Чао!");
+        parting.add("До встречи!");
+        parting.add("Гудбай!");
+        parting.add("До свидания!");
+        answersMap.put("Прощание", parting);
+        Set<String> unknown = new HashSet<>();
+        unknown.add("Я этого пока не знаю!");
+        unknown.add("Спросите, что-нибудь другое!");
+        unknown.add("Кто знает!");
+        answersMap.put("Неизвестное", unknown);
+        return answersMap;
+    }
+}
+
